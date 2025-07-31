6acb06abff4b8fcba93f06c67dff8468f1222264
Nam Danh <namddanh@gmail.com>
2025-07-31 17:17:53 -0400

    handle single selection, now working on multi selection

---
+++ reactjs-projects/src/components/accordian/index.jsx
@@ -0,0 +1,59 @@
+// Single selection
+// Multiple selection
+
+import { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId); // switches visbility of selected
+  };
+
+  const handleMultiSelection = (getCurrentId) => {
+    let copyMultiple = [...multiple];
+
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    console.log(findIndexOfCurrentId);
+  };
+
+  return (
+    <div className="wrapper">
+      <button
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        className=""
+      >
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div key={dataItem.id} className="item">
+              <div
+                // id={dataItem.id}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
